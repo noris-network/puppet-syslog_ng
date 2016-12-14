915ec546f51c5fa16a50317db83455981c4a65eb
Micha Krause <micha@noris.net>
2016-12-14 14:12:51 +0100

    copy config variables for template

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,6 +4,14 @@
 #
 class syslog_ng::config {
 
+  $version        = $::syslog_ng::version
+  $perm           = $::syslog_ng::perm
+  $dir_perm       = $::syslog_ng::dir_perm
+  $use_dns        = $::syslog_ng::use_dns
+  $use_fqdn       = $::syslog_ng::use_fqdn
+  $frac_digits    = $::syslog_ng::frac_digits
+  $keep_timestamp = $::syslog_ng::keep_timestamp
+
   concat { '/etc/syslog-ng/syslog-ng.conf':
   }
 
